a5fd78ccf7dd7cc0a0ae87290716821cc58cf16f
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-02 06:24:30 +0300

    feat(coordPage): add districts, donors & social workers pages with placeholder content

---
+++ pages/coordinator/donors/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <div
+      class="tw-w-full tw-max-h-full tw-flex tw-flex-col tw-justify-center tw-items-center"
+    >
+      <coming-soon class="tw-aspect-square tw-w-1/2" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'DonorsPage',
+  layout: 'coordinator',
+}
+</script>
