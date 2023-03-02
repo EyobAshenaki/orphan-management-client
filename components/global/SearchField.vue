23322283e269633152d789089d12023c0ae32662
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-02 06:00:55 +0300

    feat(global): add statistics & searchField component

---
+++ components/global/SearchField.vue
@@ -0,0 +1,35 @@
+<template>
+  <v-text-field
+    v-model="searchValue"
+    class="tw-max-w-xs tw-rounded-md"
+    placeholder="Search"
+    filled
+    rounded
+    dense
+  >
+    <template #prepend-inner>
+      <fa-layers class="-tw-ml-2 tw-mr-2 tw-mt-[3px] tw-text-gray-400">
+        <fa :icon="['fa', 'magnifying-glass']" />
+      </fa-layers>
+    </template>
+  </v-text-field>
+</template>
+
+<script>
+export default {
+  name: 'SearchField',
+  data() {
+    return {
+      searchValue: '',
+    }
+  },
+
+  watch: {
+    searchValue: {
+      handler: function (newVal, oldVal) {
+        this.$emit('onSearch', newVal)
+      },
+    },
+  },
+}
+</script>
---
+++ components/global/StatisticsCard.vue
@@ -0,0 +1,30 @@
+<template>
+  <div
+    class="tw-min-h-max tw-w-56 tw-flex tw-justify-center tw-items-center tw-gap-2 tw-bg-white tw-border tw-border-gray-500 tw-rounded-md tw-shadow-md tw-py-4 tw-px-2 tw-mb-4"
+  >
+    <fa-layers class="tw-w-[30%] tw-justify-self-center">
+      <fa class="tw-w-8 tw-h-8" :icon="getIcons" />
+    </fa-layers>
+    <div class="tw-w-[65%] tw-flex tw-flex-col tw-justify-center">
+      <span class="tw-text-xs tw-text-gray-700">{{ item.title }}</span>
+      <span class="tw-text-2xl tw-font-bold">{{ item.value }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'StatisticsCard',
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+  computed: {
+    getIcons() {
+      return this.item.icon ? this.item.icon : ['fa', 'circle-question']
+    },
+  },
+}
+</script>
