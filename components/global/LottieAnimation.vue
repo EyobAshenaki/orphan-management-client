3217ccf4075b11473ece9cd093a36878096ac890
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-02 05:49:25 +0300

    feat(global): add lottie animation component

---
+++ components/global/LottieAnimation.vue
@@ -0,0 +1,33 @@
+<template>
+  <lottie :options="lottieOptions" @animCreated="handleAnimation" />
+</template>
+
+<script>
+import Lottie from 'vue-lottie/src/lottie.vue'
+export default {
+  name: 'LottieAnimation',
+
+  components: {
+    Lottie,
+  },
+
+  props: {
+    item: {
+      type: Object,
+      required: true,
+    },
+  },
+
+  data() {
+    return {
+      anim: null, // for saving the reference to the animation
+      lottieOptions: { animationData: this.item.animationData.default },
+    }
+  },
+  methods: {
+    handleAnimation: function (anim) {
+      this.anim = anim
+    },
+  },
+}
+</script>
