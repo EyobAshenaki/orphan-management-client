a5272ff6512388ac64a1ac2dbec0ac7bbcd79a3c
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-02 06:05:55 +0300

    feat(coordPage): add home & settings page

---
+++ pages/coordinator/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <section>
+    <div
+      class="tw-w-full tw-max-h-full tw-flex tw-flex-col tw-justify-center tw-items-center"
+    >
+      <work-in-progress class="tw-aspect-square tw-w-1/2" />
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: 'CoordinatorPage',
+  layout: 'coordinator',
+}
+</script>
