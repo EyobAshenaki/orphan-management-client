fefb9c8839115c61430ccd97e1fb719957cefcb1
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-02 06:03:39 +0300

    feat(layout): add coordinator layout

---
+++ layouts/coordinator.vue
@@ -0,0 +1,72 @@
+<template>
+  <v-app>
+    <side-bar :items="items" />
+    <v-main class="tw-bg-gray-100">
+      <v-container class="tw-h-full">
+        <section class="tw-h-fit tw-flex tw-justify-between tw-gap-10">
+          <back-button />
+
+          <v-spacer></v-spacer>
+
+          <search-field />
+
+          <v-avatar size="40" color="grey lighten-2">
+            <fa-layers class="fa-lg">
+              <fa :icon="['fa', 'user']" />
+            </fa-layers>
+          </v-avatar>
+        </section>
+
+        <bread-crumbs />
+        <Nuxt />
+      </v-container>
+    </v-main>
+  </v-app>
+</template>
+
+<script>
+import SideBar from '~/components/layout/SideBar.vue'
+import BreadCrumbs from '~/components/layout/BreadCrumbs.vue'
+export default {
+  name: 'CoordinatorLayout',
+
+  components: {
+    SideBar,
+    BreadCrumbs,
+  },
+
+  data() {
+    return {
+      items: [
+        {
+          icon: ['fas', 'house'],
+          title: 'Home',
+          to: '/coordinator',
+        },
+        {
+          icon: ['fas', 'diagram-project'],
+          title: 'Projects',
+          to: '/coordinator/projects',
+        },
+        {
+          icon: ['fas', 'people-group'],
+          title: 'Social Workers',
+          to: '/coordinator/socialWorkers',
+        },
+        {
+          icon: ['fas', 'hand-holding-dollar'],
+          title: 'Donors',
+          to: '/coordinator/donors',
+        },
+        {
+          icon: ['fas', 'city'],
+          title: 'Districts',
+          to: '/coordinator/districts',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
