d79a692da52ddcab0ae3580d2797a447b0117e10
EyobAshenaki <ethioeyoba@gmail.com>
2023-03-02 05:57:09 +0300

    feat(global): add light and dark buttons

---
+++ components/global/ButtonDark.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    class="tw-w-fit tw-text-white tw-text-sm tw-capitalize tw-bg-emerald-600 hover:tw-bg-emerald-700"
+    depressed
+    :ripple="false"
+    :to="to"
+    @click="$emit('click')"
+  >
+    <slot> Click Me </slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'ButtonDark',
+  props: {
+    to: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
---
+++ components/global/ButtonLight.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-btn
+    class="tw-w-fit tw-text-sm tw-capitalize hover:tw-text-emerald-800 hover:tw-bg-emerald-800/10 tw-py-1 tw-px-3"
+    text
+    :ripple="false"
+    :to="to"
+    @click="$emit('click')"
+  >
+    <slot> Click Me </slot>
+  </v-btn>
+</template>
+
+<script>
+export default {
+  name: 'ButtonLight',
+  props: {
+    to: {
+      type: String,
+      default: '',
+    },
+  },
+}
+</script>
